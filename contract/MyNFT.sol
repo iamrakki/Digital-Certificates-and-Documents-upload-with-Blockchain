98afd2b4666cd55b09804d68588cb338a60bee1c
RAKKI R <93214464+iamrakki@users.noreply.github.com>
2023-08-28 12:14:00 +0530

    Update MyNFT.sol

--- contract/MyNFT.sol
+++ contract/MyNFT.sol
@@ -6,9 +6,11 @@ import "@openzeppelin/contracts/utils/Counters.sol";
 
 contract MyNFT is ERC721 {
     using Counters for Counters.Counter;
+
     Counters.Counter private _tokenIds;
 
     mapping(uint256 => string) private _ipfsHashes;
+
     mapping(uint256 => address) private _tokenOwners;
 
     constructor() ERC721("MyNFT", "NFT") {}
@@ -18,15 +20,20 @@ contract MyNFT is ERC721 {
 
         for (uint256 i = 0; i < to.length; i++) {
             _tokenIds.increment();
+
             uint256 tokenId = _tokenIds.current();
+
             _safeMint(to[i], tokenId);
+
             _ipfsHashes[tokenId] = ipfsHashes[i];
+
             _tokenOwners[tokenId] = to[i];
         }
     } 
 
     function getTokenData(uint256 tokenId) external view returns (string memory ipfsHash, address owner) {
         require(_exists(tokenId), "Token does not exist");
+
         return (_ipfsHashes[tokenId], _tokenOwners[tokenId]);
     }
 }
